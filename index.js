2383e77fd0fd38a1c328a8c5ae5066750339f423
Dawson Goodell <dawsong@osmstudios.com>
2020-04-04 19:14:23 -0700

    add prepare to package.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   module.exports = require("./lib/prod");
-//} else {
-//  module.exports = require("./lib/dev");
-//}
+} else {
+  module.exports = require("./lib/dev");
+}
